3e66c900b61b8b4c8be0686616418d75986cefd3
Vince <vincexermitano@gmail.com>
2022-02-03 16:21:23 -0800

    Create UI for AddClassesActivity

--- app/src/main/java/com/example/bofteam24/AddClassesActivity.java
+++ app/src/main/java/com/example/bofteam24/AddClassesActivity.java
@@ -3,6 +3,8 @@ package com.example.bofteam24;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddClassesActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class AddClassesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_classes);
+
+//        Spinner mySpinner = (Spinner) findViewById(R.id.year_spinner);
+//
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(AddClassesActivity.this,
+//                android.R.layout)
+
+        Spinner yearSpinner = (Spinner) findViewById(R.id.year_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.years, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        yearSpinner.setAdapter(adapter);
+
+
+        Spinner quarterSpinner = (Spinner) findViewById(R.id.quarter_spinner);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.quarters, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        quarterSpinner.setAdapter(adapter2);
+
     }
 }
