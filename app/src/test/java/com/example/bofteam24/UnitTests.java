c579dbe1074e9702eb5a0639b6a37158560ba204
kirtan-shah <kishah@ucsd.edu>
2022-02-05 17:18:35 -0800

    Add unit test for course

---
+++ app/src/test/java/com/example/bofteam24/UnitTests.java
@@ -0,0 +1,18 @@
+package com.example.bofteam24;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTests {
+    @Test
+    public void testNewCourseInfoCorrect() {
+        Course course = new Course(2022, "Winter", "CSE", "110");
+        assertEquals("CSE 110 Winter 2022", course.toString());
+    }
+}
