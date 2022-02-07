9d86913de7e0a30395a87d0d5976ee611aa6ef49
kirtan-shah <kishah@ucsd.edu>
2022-02-06 19:47:17 -0800

    Update test options

--- app/src/test/java/com/example/bofteam24/AddClassesRobolectricTests.java
+++ app/src/test/java/com/example/bofteam24/AddClassesRobolectricTests.java
@@ -12,6 +12,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 
 import static org.junit.Assert.assertEquals;
 
