0fc91e73727c4497c6a2c0ab131c1da28fc655cd
Aadhar Sharma <aadhar@aadhar.lan>
2022-02-08 00:03:51 -0800

    homepage and show list

--- app/src/main/java/com/example/bofteam24/MainActivity.java
+++ app/src/main/java/com/example/bofteam24/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.bofteam24;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //SharedPreferences sp = null;
+    //final String firstRun = "firstrun";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //sp = getSharedPreferences("com.example.bofteam24", 0);
+    }
+
+    public void onStartClick(View view) {
+        Intent intent = new Intent(this, StudentsListActivity.class);
+        startActivity(intent);
     }
 }
