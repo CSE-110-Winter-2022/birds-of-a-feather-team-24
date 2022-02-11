cc7f5ecc308db22c5534345d5c0aab3693c3ff2a
vince-ermitano <93059468+vince-ermitano@users.noreply.github.com>
2022-02-10 18:43:26 -0800

    Add DatabaseTesting.java

--- app/src/main/java/com/example/bofteam24/db/User.java
+++ app/src/main/java/com/example/bofteam24/db/User.java
@@ -7,11 +7,6 @@ import androidx.room.PrimaryKey;
 @Entity(tableName = "users")
 public class User {
 
-//    public User(int userId, String name) {
-//        this.userId = userId;
-//        this.name = name;
-//    }
-
     public User(int userId, String name, String photo_url) {
         this.userId = userId;
         this.name = name;
