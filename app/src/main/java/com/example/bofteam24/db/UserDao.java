e947903fadd3f58b58998e0a2fa4494f3e350072
kirtan-shah <kishah@ucsd.edu>
2022-02-11 18:26:38 -0800

    Update UserDao queries

--- app/src/main/java/com/example/bofteam24/db/UserDao.java
+++ app/src/main/java/com/example/bofteam24/db/UserDao.java
@@ -1,12 +1,26 @@
 package com.example.bofteam24.db;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface UserDao {
     @Query("SELECT MAX(userId) from users")
     int maxId();
 
+    @Insert
+    void insert(User user);
 
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT COUNT(*) from users")
+    int count();
+
+    @Query("SELECT * FROM users")
+    List<User> getAll();
 }
